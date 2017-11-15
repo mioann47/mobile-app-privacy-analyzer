6baa1a70c35457d7058310e84670b2b9a4679223
Modes <Modes@DESKTOP-015GMHT>
2017-11-15 21:55:57 +0200

    Added Library searching from APK

--- src/main.java
+++ src/main.java
@@ -1,13 +1,70 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.gson.Gson;
+
+import models.LibraryModel;
 
 
 
 public class main {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	
+	public static String getJSON(String url, int timeout) {
+	    HttpURLConnection c = null;
+	    try {
+	        URL u = new URL(url);
+	        c = (HttpURLConnection) u.openConnection();
+	        c.setRequestMethod("GET");
+	        c.setRequestProperty("Content-length", "0");
+	        c.setUseCaches(false);
+	        c.setAllowUserInteraction(false);
+	        //c.setConnectTimeout(timeout);
+	        //c.setReadTimeout(timeout);
+	        c.connect();
+	        int status = c.getResponseCode();
+
+	        switch (status) {
+	            case 200:
+	            case 201:
+	                BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+	                StringBuilder sb = new StringBuilder();
+	                String line;
+	                while ((line = br.readLine()) != null) {
+	                    sb.append(line+"\n");
+	                }
+	                br.close();
+	                return sb.toString();
+	        }
+
+	    } catch (MalformedURLException ex) {
+	        //Logger.getLogger(getName()).log(Level.SEVERE, null, ex);
+	    } catch (IOException ex) {
+	        //Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+	    } finally {
+	       if (c != null) {
+	          try {
+	              c.disconnect();
+	          } catch (Exception ex) {
+	             //Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+	          }
+	       }
+	    }
+	    return null;
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
 	    Process proc = Runtime.getRuntime().exec("java -jar PermissionChecker.jar apks/org.adaway_60.apk");
@@ -23,11 +80,19 @@ public class main {
 	    Gson g= new Gson();
 	   
 	    models.ApplicationPermissionsModel p = g.fromJson(data, models.ApplicationPermissionsModel.class);
-	    System.out.println(p.declared.toString());
+	    //System.out.println(p.declared.toString());
 
 	    byte c[]=new byte[err.available()];
 	    err.read(c,0,c.length);
-	    //System.out.println(new String(c));
+	    
+	    String jdata = getJSON("http://192.168.1.100:5000/apk",0);
+	    //System.out.println(jdata);
+	    
+	    LibraryModel[] array = g.fromJson(jdata, LibraryModel[].class);
+	    
+	    //System.out.println("Test="+array[1].Popularity);
+	    //System.out.println("Test="+array[3].Popularity);
+	    System.out.println("Test="+array[0].Permission.toString());
 	}
 
 }
---
+++ src/models/LibraryModel.java
@@ -0,0 +1,18 @@
+package models;
+
+import java.util.ArrayList;
+
+public class LibraryModel {
+
+public String Library;
+public String Match_Ratio;
+public String Package;
+public ArrayList<String> Permission=new ArrayList<String>();
+public String Popularity;
+public String Standard_Package;
+public String Type;
+public String Website;
+public String Weight;
+
+
+}

0552f81d0af196ddf663894fa3e780980014fe31
Modes <Modes@DESKTOP-015GMHT>
2018-01-14 13:23:30 +0200

    Weka model and predictions
    Train model and predict results
    Libraries folder created

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
---
+++ src/testClasses/WekaTest2.java
@@ -0,0 +1,51 @@
+package testClasses;
+import weka.classifiers.lazy.IBk;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.evaluation.Evaluation;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class WekaTest2 {
+
+    public static void main(String[] args) throws Exception {
+
+            Classifier zzr = new IBk(1);
+            
+            Instances train = new Instances(new BufferedReader(new FileReader("Datasets/train.arff")));
+            int lastIndex = train.numAttributes() - 1;
+            
+            train.setClassIndex(lastIndex);
+            
+            Instances test = new Instances(new BufferedReader(new FileReader("Datasets/test.arff")));
+            test.setClassIndex(lastIndex);
+            
+            zzr.buildClassifier(train);
+            
+            int counter=0;
+
+          
+          
+            weka.core.SerializationHelper.write("myModel.model", zzr);
+            Classifier cls = (Classifier) weka.core.SerializationHelper.read("myModel.model");
+            Evaluation eval = new Evaluation(train);
+            eval.evaluateModel(cls, test);
+            System.out.println(eval.toSummaryString()); ;
+            for(int i=0; i<test.numInstances(); i++) {
+        		
+            	double actualclass=test.instance(i).classValue();
+            	String actual = test.classAttribute().value((int)actualclass);
+            	Instance newInst=test.instance(i);
+            	double prednbk = zzr.classifyInstance(newInst);
+            	String predString=test.classAttribute().value((int)prednbk);
+
+                System.out.println("Actual : "+actual+" Predicted : "+predString);
+
+        }
+
+
+            
+    }
+}

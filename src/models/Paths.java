9e56be8aa5d7261a14d858593d52fb41382e5779
mioann47 <mioann47@cs.ucy.ac.cy>
2018-03-10 01:37:46 +0200

    Path file added

---
+++ src/models/Paths.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Paths {
+
+	
+	public static String liteRadarPath="mypythonscripts/literadar.py";
+	public static String permissionCheckerPath="PermissionChecker.jar";
+	public static String pythonScript="mypythonscripts/myscript.py";
+	public static String wekaModelPath="myModel.model";
+	public static String wekaAttributesPath="Datasets/myAttributes.arff";
+	
+	
+}
